09970106f616afd67d2d94c34e1b6097d0c5ad5d
Kazuko <foolidiotnarr@gmail.com>
2021-06-10 20:51:00 +0800

    做了很多東西

--- Web410716021/ViewModels/SignUpData.cs
+++ Web410716021/ViewModels/SignUpData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,23 @@ namespace Web410716021.ViewModels
 {
     public class SignUpData
     {
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage = "請輸入性名")]
+        [MinLength(2, ErrorMessage = "請輸入2-10個字")]
+        [MaxLength(10, ErrorMessage = "請輸入2-10個字")]
         public string Name { get; set; }
+
+
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        [EmailAddress(ErrorMessage = "請輸入正確的email格式")]
         public string Account { get; set; }
+
+
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        [StringLength(12, MinimumLength = 6 ,ErrorMessage = "請輸入6-12個字" )]
         public string Password { get; set; }
-        public string NameMessage { get; set; }
-        public string AccountMessage { get; set; }
-        public string PasswordMessage { get; set; }
         public string Message { get; set; }
     }
 }
